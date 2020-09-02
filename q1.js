210361631d76ec4e04454341841b9646699eff05
sirmtgo <sirmtgo@gmail.com>
2020-09-02 00:54:38 -0300

    2° commit

--- q1.js
+++ q1.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
+const moedaModel = require('./moedaModel')
 
 app.get('/alunos/todos', function(req, res){
     let alunos = [
-        {'aluno1': 'Mario'}, 
-        {'aluno2': 'Julia'}, 
-        {'aluno3': 'Poliana'}
+        {'nome': 'Mario'}, 
+        {'nome': 'Julia'}, 
+        {'nome': 'Poliana'}
     ]
     res.json(alunos)
 })
 
-app.get('/moeda/:valor_real', function(req, res){
-
-})
+app.get('/moeda/:valor_real', moedaModel.converterValor)
 
 app.listen(3000)
